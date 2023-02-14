d733a05db4b655cdb3c0509bceaae718cf2e4f68
Andrey Sushkov <>
2023-02-14 12:25:33 +0300

    Initial commit

---
+++ Bank-cards-on-SnapKit-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank-cards-on-SnapKit-sample
+//
+//  Created by Andrei Sushkou on 14.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
